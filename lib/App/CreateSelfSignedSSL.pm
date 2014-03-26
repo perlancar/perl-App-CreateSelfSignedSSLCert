d761342805ae30761d2353887607543088b75fb8
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-03-26 19:56:29 +0700

    Initial commit

---
+++ lib/App/CreateSelfSignedSSL.pm
@@ -0,0 +1,13 @@
+package App::CreateSelfSignedSSL;
+
+# VERSION
+
+1;
+# ABSTRACT: Create self-signed SSL certificate
+
+=head1 SYNOPSIS
+
+This distribution provides command-line utility called
+L<create-self-signed-ssl-cert>.
+
+=cut
